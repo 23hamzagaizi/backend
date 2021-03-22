715884c94a3e30206065d8442bb1475509b0f585
Hamza <hamza.gaizi@hotmail.com>
2021-03-22 21:16:24 +0000

    authentification + first HW

---
+++ MyShop-main/Data/Context.cs
@@ -0,0 +1,16 @@
+using Microsoft.EntityFrameworkCore;
+using Models;
+using MyShop.Models;
+
+namespace MyShop.Data
+{
+    public class Context : DbContext
+    {
+        public Context(DbContextOptions<Context> options) : base(options){}
+
+        public DbSet<Customer> Customer { get; set; }
+        public DbSet<Order> Order { get; set; }
+        public DbSet<Product> Product { get; set; }
+        public DbSet<User> User{get; set;}
+    }
+}
